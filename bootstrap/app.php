59996e509e2599fa8069e7fd495dcf64b0a3457f
wind-fire <877400859.qq.com>
2018-08-10 06:43:57 +0000

    modify app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@
 |
 */
 
+require __DIR__.'/helpers.php';
+
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
@@ -51,7 +53,7 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
-require __DIR__.'/helpers.php';
+
 
 
 return $app;
